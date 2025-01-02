480a34f91371e3a4fe6b958d0f9c2321ae630abe
Sandeep Sahu <Sandeep.Sahu@motherson.com>
2025-01-02 11:22:42 +0530

    modified:   FrontEnd/src/components/footer.jsx

--- FrontEnd/src/components/footer.jsx
+++ FrontEnd/src/components/footer.jsx
@@ -2,7 +2,7 @@ import { Typography } from "@material-tailwind/react";
  
 export function SimpleFooter() {
   return (
-    <footer className="flex px-10 dark:from-black dark:to-[#23252b] dark:bg-gradient-to-b flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-orange-gray-50 py-6  dark:border-black text-center md:justify-between">
+    <footer className="flex px-10 dark:from-black dark:to-[#23252b] dark:bg-gradient-to-b flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-orange-gray-50 py-6  dark:border-black text-center md:justify-between border-t-white">
       <Typography color="orange-gray" className="font-normal dark:text-white">
         &copy; 2024 BingeQL
       </Typography>
