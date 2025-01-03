0fdfd54bdc79ced69ff110167df937904b7b1caf
Sandeep Sahu <Sandeep.Sahu@motherson.com>
2025-01-03 18:51:14 +0530

    modified:   FrontEnd/src/components/Home.jsx

--- FrontEnd/src/components/Home.jsx
+++ FrontEnd/src/components/Home.jsx
@@ -24,6 +24,11 @@ const Home = () => {
       </div>
       <DataCorosal tag={'action'} />
       <div className="text-left px-10 bg-gray-200 md:bg-gray-300 pt-10 pb-5 dark:bg-black dark:md:bg-black space-y-1 md:space-y-2">
+        <h1 className="text-2xl md:text-3xl font-semibold dark:text-white">Experience Thrillers</h1>
+        <h2 className='md:text-md dark:text-gray-400'>Start your binge before the new season begins!</h2>
+      </div>
+      <DataCorosal tag={'thriller'} />
+      <div className="text-left px-10 bg-gray-200 md:bg-gray-300 pt-10 pb-5 dark:bg-black dark:md:bg-black space-y-1 md:space-y-2">
         <h1 className="text-2xl md:text-3xl font-semibold dark:text-white">Explore Romance</h1>
         <h2 className='md:text-md dark:text-gray-400'>Start your binge before the new season begins!</h2>
       </div>
