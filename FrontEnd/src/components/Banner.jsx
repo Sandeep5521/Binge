75b21ec9a0ae60307ff4c6ac3b09f37629eb5773
Sandeep Sahu <Sandeep.Sahu@motherson.com>
2025-02-16 18:53:46 +0530

    modified:   FrontEnd/src/components/Banner.jsx

--- FrontEnd/src/components/Banner.jsx
+++ FrontEnd/src/components/Banner.jsx
@@ -7,51 +7,110 @@
 
 // import React from 'react'
 // import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y,Keyboard,Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y, Keyboard, Autoplay } from 'swiper/modules';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { parent } from '../App';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/bundle'
+import { useContext, useEffect, useState } from 'react';
 
 const Banner = () => {
-    if (document.getElementById('aud')) {
-        const aud = document.getElementById('aud');
-        aud.volume = 0.1;
-    }
-    return (
-        // <>
-        //     <video className='w-screen object-fill ' autoPlay loop>
-        //         <source src={`${Bvideo}`} type='video/mp4' />
-        //     </video>
-        //     <audio id='aud' src={`${Bsound}`} loop autoPlay></audio>
-        // </>
-        <Swiper
+  const [List, setList] = useState([]);
+  const gotoComp = useContext(parent);
+  const getList = async () => {
+    let tmp = await fetch('https://bingeql.onrender.com/graphQL', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        query: `query ExampleQuery($page: Int, $limit: Int,$tag: String) {
+              Movies(page: $page, limit: $limit, tag: $tag) {
+                  _id,
+                  movieName,
+                  movieThumbnail
+              }
+            }`,
+        variables: {
+          page: 1,
+          limit: 6,
+          tag: null
+        }
+      })
+    });
+    tmp = await tmp.json();
+    console.log('ss4 =>', tmp);
+    setList(tmp.data.Movies);
+  }
+
+  useEffect(()=>{
+    getList();
+  },[])
+
+  if (document.getElementById('aud')) {
+    const aud = document.getElementById('aud');
+    aud.volume = 0.1;
+  }
+  return (
+    // <>
+    //     <video className='w-screen object-fill ' autoPlay loop>
+    //         <source src={`${Bvideo}`} type='video/mp4' />
+    //     </video>
+    //     <audio id='aud' src={`${Bsound}`} loop autoPlay></audio>
+    // </>
+    <Swiper
       // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y,Keyboard,Autoplay]}
+      modules={[Navigation, Pagination, Scrollbar, A11y, Keyboard, Autoplay]}
       spaceBetween={0}
       slidesPerView={1}
+      breakpoints={{
+        640: {
+          slidesPerView: 2
+        },
+        768: {
+          slidesPerView: 3
+        },
+        1024: {
+          slidesPerView: 4
+        },
+      }}
       navigation
-      autoplay = {true}
+      autoplay={true}
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
-      keyboard = {{enabled:true,onlyInViewport:false}}
+      keyboard={{ enabled: true, onlyInViewport: false }}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
+      className='bg-black h-[30rem]'
       effect='fade'
     >
-      <SwiperSlide>
+      {/* <SwiperSlide>
         <div className='flex justify-center'>
           <img src="https://wallpapercave.com/wp/wp8969483.jpg" alt="" className='object-fit' />
         </div>
-      </SwiperSlide>
+      </SwiperSlide> */}
+
+      {List.map((cur) => {
+        return (
+          <SwiperSlide key={cur._id}>
+            <div className='flex justify-center cursor-pointer hover:brightness-75' onClick={()=>gotoComp({
+                    display: 'page',
+                    data: cur._id
+                })}>
+              <img src={cur.movieThumbnail} alt="" className='object-fit' />
+            </div>
+          </SwiperSlide>
+        )
+      })}
       {/* <SwiperSlide>
         <div className='flex justify-center'>
           <img src="https://wallpapercave.com/wp/wp11391382.png" alt="" className='object-fit' />
         </div>
       </SwiperSlide> */}
-      <SwiperSlide>
+      {/* <SwiperSlide>
         <div className='flex justify-center'>
           <img src="https://wallpapercave.com/wp/wp10456761.jpg" alt="" className='object-fit' />
         </div>
@@ -60,9 +119,9 @@ const Banner = () => {
         <div className='flex justify-center'>
           <img src="https://wallpapercave.com/wp/wp14549974.jpg" alt="" className='object-fit' />
         </div>
-      </SwiperSlide>
+      </SwiperSlide> */}
     </Swiper>
-    );
+  );
 };
 
 export default Banner
