70596862b973a4d067c7dec657d79f317e2370bd
Sandeep Sahu <Sandeep.Sahu@motherson.com>
2025-03-20 11:23:01 +0530

    modified:   FrontEnd/src/components/Nav.jsx

--- FrontEnd/src/components/Nav.jsx
+++ FrontEnd/src/components/Nav.jsx
@@ -295,16 +295,16 @@ export default function NavbarSimple() {
           </div>
           <IconButton
             variant="text"
-            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden hover:cursor-default"
             ripple={false}
-            onClick={() => {
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6 dark:text-white text-black absolute -right-20 -top-3 cursor-pointer hover:scale-105" onClick={() => {
               if (openNav) setOpenNav(false);
               console.log(typeof openNav)
               gotoComp({
                 display: 'search'
               });
             }}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6 dark:text-white text-black absolute -right-20 -top-3 cursor-pointer hover:scale-105">
               <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clip-rule="evenodd" />
             </svg>
           </IconButton>
