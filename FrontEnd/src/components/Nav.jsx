95e6c9548705ca76b8c29ab45698e1663e070b28
Sandeep Sahu <Sandeep.Sahu@motherson.com>
2025-05-03 19:32:15 +0530

    modified:   FrontEnd/src/components/Nav.jsx

--- FrontEnd/src/components/Nav.jsx
+++ FrontEnd/src/components/Nav.jsx
@@ -300,7 +300,7 @@ export default function NavbarSimple() {
             ripple={false}
             style={{WebkitTapHighlightColor: 'transparent'}}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6 dark:text-white text-black absolute -right-20 -top-3 sm:-right-72 cursor-pointer hover:scale-105" onClick={() => {
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6 dark:text-white text-black absolute -right-20 -top-3 sm:-right-64 cursor-pointer hover:scale-105" onClick={() => {
               if (openNav) setOpenNav(false);
               console.log(typeof openNav)
               gotoComp({
